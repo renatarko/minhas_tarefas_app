94ff5ce2c332e7c2b7c04172a33224df9f362cec
Renata Karonlina <renatakarolinarko@gmail.com>
2022-09-05 17:00:03 -0400

    adcionando Context_api

---
+++ src/context/TaskProvider.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const TaskContext = React.createContext()
+
+export const TaskProvider = ({children}) => {
+
+  const [newTask, setNewTask] = React.useState("")
+
+  const createTask = (task) => { 
+    setNewTask(task)
+  }
+
+  const value = {
+    newTask,
+    createTask
+  }
+    
+  return <TaskContext.Provider value={value}>{children}</TaskContext.Provider>
+}
